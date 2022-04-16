21dcab6b1e0708af78a2959b96bbba05b62a55bc
RedX <director099@gmail.com>
2022-04-17 00:05:11 +0500

    Валидация

--- build/js/custom.js
+++ build/js/custom.js
@@ -147,9 +147,11 @@
           const tel = new Inputmask({
             mask: "+7 (999) 999 99 99",
             showMaskOnHover: false
-          }).mask(document.querySelectorAll("[type='tel']")); // if (!tel.isComplete()) {
-          //   _this.addInvalid(elem);
-          // }
+          }).mask(document.querySelectorAll("[type='tel']"));
+
+          if (!tel.isComplete()) {
+            _this.addInvalid(elem);
+          }
         }
 
         if ($(this).data('type') == "password") {
